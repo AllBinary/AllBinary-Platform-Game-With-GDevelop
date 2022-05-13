616d652ca23046f4d1ca4f975fc29a67fdd415e5
Travis Berthelot <travisberthelot@allbinary.com>
2022-05-12 19:59:41 -0500

    The platform specific package does not need gd.

--- platform/j2se/GDGameJ2SEJavaLibraryM/src/main/java/org/allbinary/game/init/GDGameJ2MEAnimationInterfaceFactoryResourceInitialization.java
+++ platform/j2se/GDGameJ2SEJavaLibraryM/src/main/java/org/allbinary/game/init/GDGameJ2MEAnimationInterfaceFactoryResourceInitialization.java
@@ -11,7 +11,7 @@
 * Created By: Travis Berthelot
 * 
 */
-package org.allbinary.game.gd.init;
+package org.allbinary.game.init;
 
 import org.allbinary.animation.image.GDGameImageBasedAnimationInterfaceFactoryInterfaceFactory;
 import org.allbinary.animation.image.GDGameOpenGLESImageBasedAnimationInterfaceFactoryInterfaceFactory;
