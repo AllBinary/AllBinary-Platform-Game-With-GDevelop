c921b63e157037f608eafded686efc89bf9243e6
Travis Berthelot <travisberthelot@allbinary.com>
2023-06-13 20:35:36 -0500

    temp

---
+++ GDGameThreedBaseJavaLibraryM/src/main/java/org/allbinary/game/canvas/TitleVectorData.java
@@ -0,0 +1,30 @@
+/*
+* AllBinary Open License Version 1
+* Copyright (c) 2011 AllBinary
+* 
+* By agreeing to this license you and any business entity you represent are
+* legally bound to the AllBinary Open License Version 1 legal agreement.
+* 
+* You may obtain the AllBinary Open License Version 1 legal agreement from
+* AllBinary or the root directory of AllBinary's AllBinary Platform repository.
+* 
+* Created By: Travis Berthelot
+* 
+*/
+package org.allbinary.game.canvas;
+
+public class TitleVectorData 
+{
+   public TitleVectorData()
+   {
+   }
+   
+   public final int[][] testPoints = {
+       {36,1},{48,1},{48,4},{39,4},{39,9},{48,9},{48,20},{36,20},{36,17},{46,17},{46,12},{36,12},{36,1},{1000,1000},{1,1},{18,1},{18,4},{10,4},{10,20},{8,20},{8,4},{1,4},{1,1},{1000,1000},{21,1},{33,1},{33,4},{24,4},{24,9},{29,9},{29,12},{24,12},{24,17},{33,17},{33,20},{21,20},{21,1},{1000,1000},{51,1},{67,1},{67,4},{60,4},{60,20},{58,20},{58,4},{51,4},{51,1},{1000,1000}
+   };
+
+   public final int[][] gamePoints = {
+       {27,6},{24,17},{31,17},{29,6},{27,6},{1000,1000},{15,6},{15,1},{1,1},{1,28},{15,28},{15,15},{6,15},{6,22},{8,18},{13,18},{13,25},{3,25},{3,4},{13,4},{13,8},{15,8},{15,5},{1000,1000},{64,1},{64,28},{78,28},{78,25},{67,25},{67,18},{74,18},{74,15},{67,15},{67,4},{78,4},{78,1},{65,1},{1000,1000},{18,28},{26,1},{30,1},{36,28},{33,28},{32,22},{23,22},{21,28},{18,28},{1000,1000},{39,28},{45,2},{47,2},{50,17},{54,2},{56,2},{60,28},{58,28},{55,9},{54,9},{51,23},{49,23},{46,9},{45,9},{41,28},{39,28},{1000,1000}
+   };
+   
+}
