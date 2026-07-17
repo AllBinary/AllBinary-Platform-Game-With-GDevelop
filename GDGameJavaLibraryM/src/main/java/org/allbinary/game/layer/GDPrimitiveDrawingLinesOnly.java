e83fe14699c625bcd383d31139d19bdabfeb92d7
Travis Berthelot <travisberthelot@allbinary.com>
2026-07-16 22:07:12 -0500

    Optional thickness

--- GDGameJavaLibraryM/src/main/java/org/allbinary/game/layer/GDPrimitiveDrawingLinesOnly.java
+++ GDGameJavaLibraryM/src/main/java/org/allbinary/game/layer/GDPrimitiveDrawingLinesOnly.java
@@ -37,6 +37,7 @@ public class GDPrimitiveDrawingLinesOnly extends Animation {
     private final Animation colorAnimation = new Animation() {};
     private final LinePathAnimation linePathAnimation = new LinePathAnimation();
     private final BasicArrayList pointList = new BasicArrayListD();
+    //private final BasicArrayList strokeList = new BasicArrayListD();
     
     @Override
     public void nextFrame() {
@@ -52,10 +53,11 @@ public class GDPrimitiveDrawingLinesOnly extends Animation {
         this.colorAnimation.setBasicColorP(basicColor);
     }
 
-    public void addLineV2(final int x, final int y, final int x2, final int y2) {
+    public void addLineV2(final int x, final int y, final int x2, final int y2, final int thickness) {
 
         this.pointList.add(this.pointFactory.createXY(x, y));
         this.pointList.add(this.pointFactory.createXY(x2, y2));
+        //this.strokeList.add(Integer.valueOf(thickness));
         //logUtil.put(new StringMaker().append("addFillRectangle: ").append(x).append(CommonSeps.getInstance().COMMA).append(y).append(CommonSeps.getInstance().COMMA).append(x2).append(CommonSeps.getInstance().COMMA).append(y2).toString(), this, commonStrings.PROCESS);
         //logUtil.put("addFillRectangle: " + this.animationListArray[this.circularIndexUtil.getIndex()].size(), this, commonStrings.PROCESS);
     }
@@ -68,10 +70,15 @@ public class GDPrimitiveDrawingLinesOnly extends Animation {
         final int size = this.pointList.size();
         GPoint point;
         GPoint nextPoint;
+        //Integer thickness;
         for(int index = 1; index < size; index+=2) {
 
             point = (GPoint) this.pointList.get(index - 1);
             nextPoint = (GPoint) this.pointList.get(index);
+            //thickness = (Integer) this.strokeList.get(index / 2);
+            //Check if JSR 226 Graphics2D is available
+            //(graphics instanceof Graphics2D)
+            //graphics.setStroke(thickness);
             this.linePathAnimation.paint(graphics, point, nextPoint, this.NULL_ALLBINARY_LAYER);
         }
 
