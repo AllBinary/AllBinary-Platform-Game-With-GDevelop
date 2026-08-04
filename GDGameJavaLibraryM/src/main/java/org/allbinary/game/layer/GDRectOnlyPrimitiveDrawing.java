ba392fa8ada2402efc1b3cc9e899f89a5657edab
Travis Berthelot <travisberthelot@allbinary.com>
2026-08-04 10:44:03 -0500

    Remove the duplication.

--- GDGameJavaLibraryM/src/main/java/org/allbinary/game/layer/GDRectOnlyPrimitiveDrawing.java
+++ GDGameJavaLibraryM/src/main/java/org/allbinary/game/layer/GDRectOnlyPrimitiveDrawing.java
@@ -15,8 +15,9 @@ package org.allbinary.game.layer;
 
 import javax.microedition.lcdui.Graphics;
 import org.allbinary.animation.Animation;
-import org.allbinary.animation.vector.ARectangleFilledAnimation;
+import org.allbinary.animation.vector.RectangleFilledAnimation;
 import org.allbinary.graphics.color.BasicColor;
+import org.allbinary.graphics.color.BasicColorFactory;
 
 import org.allbinary.logic.communication.log.LogUtil;
 
@@ -29,7 +30,10 @@ public class GDRectOnlyPrimitiveDrawing extends Animation {
 
     
     private final Animation colorAnimation = new Animation() {};
-    private final ARectangleFilledAnimation rectangleFilledAnimation = new ARectangleFilledAnimation();
+    private final RectangleFilledAnimation rectangleFilledAnimation = new RectangleFilledAnimation(0, 0, BasicColorFactory.getInstance().BLACK);
+    
+    public int x;
+    public int y;
     
     @Override
     public void nextFrame() {
@@ -43,10 +47,10 @@ public class GDRectOnlyPrimitiveDrawing extends Animation {
 
     public void addFillRectangle(final int x, final int y, final int x2, final int y2) {
         
-        this.rectangleFilledAnimation.x = x;
-        this.rectangleFilledAnimation.y = y;
-        this.rectangleFilledAnimation.width = x2 - x;
-        this.rectangleFilledAnimation.height = y2 - y;
+        this.x = x;
+        this.y = y;
+        this.rectangleFilledAnimation.setWidth(x2 - x);
+        this.rectangleFilledAnimation.setHeight(y2 - y);
         
         //logUtil.put(new StringMaker().append("addFillRectangle: ").append(x).append(CommonSeps.getInstance().COMMA).append(y).append(CommonSeps.getInstance().COMMA).append(x2).append(CommonSeps.getInstance().COMMA).append(y2).toString(), this, commonStrings.PROCESS);
         //logUtil.put("addFillRectangle: " + this.animationListArray[this.circularIndexUtil.getIndex()].size(), this, commonStrings.PROCESS);
@@ -55,8 +59,9 @@ public class GDRectOnlyPrimitiveDrawing extends Animation {
     @Override
     public void paintXY(final Graphics graphics, final int x, final int y)
     {
-        this.colorAnimation.paintXY(graphics, x, y);
-        this.rectangleFilledAnimation.paintXY(graphics, x, y);
+        //Primitive are not tied to the Game Object location.
+        this.colorAnimation.paintXY(graphics, this.x, this.y);
+        this.rectangleFilledAnimation.paintXY(graphics, this.x, this.y);
     }
 
     @Override
